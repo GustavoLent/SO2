02260385c9e8127a11f039e2bb905b2f371aaa8e
Gustavo <gustavo.lent@unesp.br>
2022-07-14 22:31:57 -0300

    alterações na identação do código C + definição do tamanho de FolderContent no diagrama

--- simulador-sistema-arquivos/simulador.c
+++ simulador-sistema-arquivos/simulador.c
@@ -4,22 +4,20 @@
 // Trabalho 02 - Simulador de sistema de arquivos
 // Sistemas Operacionais II - Davi Duarte de Paula
 
-// alô
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-int mkdir_exec (char *name, char *size); // Criar diret�rio (nome)
-int mkfile_exec(char *name, char *size); // Criar arquivo (nome, tamanho)
-int rmdir_exec(char *name, char *size); // Excluir um diret�rio (nome)
-int rmfile_exec(char *name, char *size); // RMFILE: Excluir um arquivo (nome)
-int ls_exec(char *name, char *size); // LS: Listar arquivos e diret�rios
-int showset_exec(char *name, char *size); // SHOWSET: Mostrar setores ocupados pelo arquivo (nome)
-int lset_exec(char *name, char *size); // LSET: Mostrar mapa de setores livres e ocupados
-int help_exec(char *name, char *size); // HELP: Exibir o guia de ajuda
-int exit_exec(char *name, char *size); // EXIT: 
+int mkdir_exec (char *name, char *size);	// Criar diretório (nome)
+int mkfile_exec(char *name, char *size);	// Criar arquivo (nome, tamanho)
+int rmdir_exec(char *name, char *size);		// Excluir um diretório (nome)
+int rmfile_exec(char *name, char *size);	// RMFILE: Excluir um arquivo (nome)
+int ls_exec(char *name, char *size); 		// LS: Listar arquivos e diretórios
+int showset_exec(char *name, char *size); 	// SHOWSET: Mostrar setores ocupados pelo arquivo (nome)
+int lset_exec(char *name, char *size); 		// LSET: Mostrar mapa de setores livres e ocupados
+int help_exec(char *name, char *size); 		// HELP: Exibir o guia de ajuda
+int exit_exec(char *name, char *size); 		// EXIT: 
 
 struct actions {
   char *cmd;
@@ -37,44 +35,40 @@ struct actions {
     {NULL,NULL}
 };
 
-int mkdir_exec(char *name, char *size)
-{
+int mkdir_exec(char *name, char *size) {
 	return 0;
 }
-int mkfile_exec(char *name, char *size)
-{
+
+int mkfile_exec(char *name, char *size) {
 	return 0;
 }
-int rmdir_exec(char *name, char *size)
-{
+
+int rmdir_exec(char *name, char *size) {
 	return 0;
 }
-int rmfile_exec(char *name, char *size)
-{
+
+int rmfile_exec(char *name, char *size) {
 	return 0;
 }
-int ls_exec(char *name, char *size)
-{
+
+int ls_exec(char *name, char *size) {
 	return 0;
 }
-int showset_exec(char *name, char *size)
-{
+
+int showset_exec(char *name, char *size) {
 	return 0;
 }
-int lset_exec(char *name, char *size)
-{
+
+int lset_exec(char *name, char *size) {
 	return 0;
 }
-int help_exec(char *name, char *size)
-{
+
+int help_exec(char *name, char *size) {
 	return 1;
 }
-int exit_exec(char *name, char *size)
-{
+int exit_exec(char *name, char *size) {
 	return 0;
 }
-
-
 
 int main ()
 {
@@ -84,52 +78,42 @@ int main ()
   int i, search;
   char *cmd;
 
-
-
-  while (1)
-    {
+  	while (1) {
     	printf ("[SisArq] $ ");
 
     	// captura o comando digitado
     	fgets (line, sizeof (line), stdin);
 
-
     	// processa o enter sozinho
     	if (line[0] == '\n') continue;
-
 
     	// substitui o enter por fim de linha
     	for (i = 0; line[i] != '\n'; i++);
     	line[i] = '\0';
 
-
     	// separa a linha digitada em tokens
     	i = 0;
     	token = strtok (line, " ");
-    	while (token != NULL)
-		{
+    	while (token != NULL) {
 	  		args[i++] = token;
 	  		token = strtok (NULL, " ");
 		}
       	args[i++] = NULL;
 
-
 		// processa os comandos digitados
 		search = 0;
-      	for (struct actions *ptr = cmd_list; ptr->cmd != NULL; ptr++)
-		{ 
-        	if (strcmp(ptr->cmd, args[0]) == 0)
-            {
+      	for (struct actions *ptr = cmd_list; ptr->cmd != NULL; ptr++) { 
+        	if (strcmp(ptr->cmd, args[0]) == 0) {
                 search = 1;
                 if ((ptr->actions)(args[1], args[2]) != 0)
 				fprintf (stderr, "[SisArq] %s %s %s: failed\n", args[0], args[1], args[2]);
 			}
 		}
 		
-     	if (search == 0) printf("[SisArq] not found: %s\n", args[0]);
+     	if (search == 0)  printf("[SisArq] not found: %s\n", args[0]); 
 
 	}
-		return 0;
+	return 0;
 
 }
 
